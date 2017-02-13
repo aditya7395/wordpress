22d21c2f561730d84febf94d58aaf9be91e91c16
Duy <duy.nguyen07@sjsu.edu>
2017-02-13 13:56:08 -0800

    Added support for Ticket Link attribute name

--- plugins/events-manager-ha-custom/js/events-manager-ha-custom.js
+++ plugins/events-manager-ha-custom/js/events-manager-ha-custom.js
@@ -5,27 +5,45 @@ var EMHaCustom = (function($, window) {
 	/**
  	  * Generate html for ticket button
 	  * @param {Object} options - {
-	  * 		attr_name: "Ovationtix Link",
-	  * 		ticketLink: {String}
-	  *		btnImgLink: {String}
+	  * 			attr_name: "Ovationtix Link",
+	  * 			ticketLink: {String},
+	  *				btnImgLink: {String}, - only required for Ovationtix Link
+	  *				btnName: {String} - default to "Buy Ticket"
+	  *         }
 	  * @return {HTML|false}	  
 	*/
 	public.generateTicketBtn = function(options) {
+		if(!options.ticketLink) {
+			console.log("ERROR: Undefined ticketLink");
+			return false;
+		}
+
 		if(options.attr_name === 'Ovationtix Link') {
-			if(!options.ticketLink || !options.btnImgLink) {
-				console.log("ERROR: Undefined ticketLink or btnImgLink");
-				return false;
-			}	
+
+			if(!options.btnImgLink) {
+				console.log("ERROR: Undefined btnImgLink");
+				return false;	
+			}
+
 			var template =  '<a href=\'' + options.ticketLink + '\'>' +
-				'<img src=\'' + options.btnImgLink + '\' alt="Buy Ticket"></img>' +
+					'<img src=\'' + options.btnImgLink + '\' alt="Buy Ticket"></img>' +
 				'</a>';
 				
 			return $.parseHTML(template)[0];			
+		} else if(options.attr_name === 'Ticket Link') {
+
+			options.btnName = options.btnName || "Buy Ticket";
+
+			var template =  '<a href=\'' + options.ticketLink + '\'>' +
+					'<button>' + options.btnName + '</button>' +
+				'</a>';
+				
+			return $.parseHTML(template)[0];
 		} else {
 			//console.log('ERROR: Unknown attribute name', options.attr_name);
 			return false;
 		}
-	}	
+	}
 
 	return public;
 })(jQuery, window);
--- plugins/events-manager-ha-custom/js/test/ha-custom.test.js
+++ plugins/events-manager-ha-custom/js/test/ha-custom.test.js
@@ -8,16 +8,7 @@ var assert = chai.assert;
 */
 describe('ha_custom', function() {
 	describe('generateTicketBtn', function() {
-		var defaultAttrName = "Ovationtix Link";
-		it('should return html that matches template', function(){
-			var data = {
-				attr_name: defaultAttrName,
-				ticketLink: "Default ticket link",
-				btnImgLink: "Default btn image link"
-			};
-			var res = EMHaCustom.generateTicketBtn(data);
-			assert.equal(res.outerHTML, '<a href="Default ticket link"><img src="Default btn image link" alt="Buy Ticket"></a>');
-		});
+		var ovationtixAttrName = "Ovationtix Link";
 
 		it('should return error when given bad attr_name', function(){
 			var data = {
@@ -31,24 +22,69 @@ describe('ha_custom', function() {
 		
 		it('should return error when given empty ticket link', function(){
 			var data = {
-				attr_name: defaultAttrName,
+				attr_name: ovationtixAttrName,
 				ticketLink: "",
 				btnImgLink: "Default btn image link"
 			};
 			var res = EMHaCustom.generateTicketBtn(data);
 			assert.isNotOk(res, 'empty ticket link');
 		});
-		it('should return error when given empty button image link', function(){
-			var data = {
-				attr_name: defaultAttrName,
-				ticketLink: "Default ticket link",
-				btnImgLink: ""
-			};
-			var res = EMHaCustom.generateTicketBtn(data);
-			assert.isNotOk(res, 'empty button image link');
+		
+		describe('Ovationtix Link Attribute', function() {
+
+			it('should return error when given empty button image link', function(){
+				var data = {
+					attr_name: ovationtixAttrName,
+					ticketLink: "Default ticket link",
+					btnImgLink: ""
+				};
+				var res = EMHaCustom.generateTicketBtn(data);
+				assert.isNotOk(res, 'empty button image link');
+			});
+
+			it('should return html that matches template', function(){
+				var data = {
+					attr_name: ovationtixAttrName,
+					ticketLink: "Default ticket link",
+					btnImgLink: "Default btn image link"
+				};
+				var res = EMHaCustom.generateTicketBtn(data);
+				assert.equal(res.outerHTML, '<a href="Default ticket link"><img src="Default btn image link" alt="Buy Ticket"></a>');
+			});
 		});
+		
+		describe('Ticket Link Attribute', function() {
+			var ticketLinkAttrName = "Ticket Link";
 
+			it('should return html that matches template', function(){
+				var data = {
+					attr_name: ticketLinkAttrName,
+					ticketLink: "Default ticket link"
+				};
+				var res = EMHaCustom.generateTicketBtn(data);
+				assert.equal(res.outerHTML, '<a href="Default ticket link"><button>Buy Ticket</button></a>');
+			});
 
+			describe('options.btnName', function() {
+				it("should default to 'Buy Ticket' when btnName is undefined", function() {
+					var data = {
+						attr_name: ticketLinkAttrName,
+						ticketLink: "Default"
+					}
+					var res = EMHaCustom.generateTicketBtn(data);
+					assert.equal(res.outerHTML, '<a href="Default"><button>Buy Ticket</button></a>');
+				});
+				it("should show the text in btnName when it is defined", function() {
+					var data = {
+						attr_name: ticketLinkAttrName,
+						ticketLink: "Default",
+						btnName: "Extra Ticket"
+					}
+					var res = EMHaCustom.generateTicketBtn(data);
+					assert.equal(res.outerHTML, '<a href="Default"><button>Extra Ticket</button></a>');
+				});
+			});
+		});
 	});
 });	
 
