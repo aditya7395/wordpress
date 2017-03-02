ccbab5ac25e6cf312ced535ef0ab04633d45cc36
Graphic_01 <graphic01@CoHas-iMac-4.local>
2017-03-01 16:34:32 -0800

    Added javascript to redirect user to back to where to they were when they click login. Documentation will be added later

--- plugins/ha-custom-login/js/ha-custom-login.js
+++ plugins/ha-custom-login/js/ha-custom-login.js
@@ -1,1 +1,16 @@
-console.log("ha custom login");
+/*
+* This scripts enable the class 'ha-custom-login' which adds an eventlistener to redirect user to the login page with a redirect url
+* To use - simply add the ha-custom-login class to a login link on the page.
+*/
+(function($, window) {
+	$( document ).ready(function() {
+		var currentUrl = encodeURI( window.location.href ),
+			currentOrigin = window.location.origin,
+			loginUrl = '/wp-login.php';
+
+	    $('.ha-custom-login').click(function(e) {
+			e.preventDefault();
+			window.location = [currentOrigin, loginUrl, '?redirect_to=', currentUrl, '&reauth=1'].join('');
+		});
+	});
+})(jQuery, window);
