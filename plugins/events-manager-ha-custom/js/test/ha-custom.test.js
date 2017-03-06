94c2664c2983ae414c8e50a300313bc9ca818872
Messi Chaib <messi.chaib@sjsu.edu>
2017-03-06 13:12:05 -0800

    generateYoutubeLink function format was changed

--- plugins/events-manager-ha-custom/js/test/ha-custom.test.js
+++ plugins/events-manager-ha-custom/js/test/ha-custom.test.js
@@ -59,11 +59,11 @@ describe('ha_custom', function() {
 			it('should return youtube link that matches template', function(){
 				var data = {
 					attr_name: youtubeLinkAttrName,
-					youtubeLink: 'http://www.messichaib.com'
+					youtubeLink: 'https://www.youtube.com/watch?v=_pwdixReIZ4'
 				};
 				
 				var res = EMHaCustom.generateYoutubeLink(data);
-				assert.equal(res.outerHTML, '<div style="position:relative;height:0;padding-bottom:56.25%"><iframe class="youtubeLink" width="500" rameborder="0" src="http://www.messichaib.com"></iframe></div>');
+				assert.equal(res.outerHTML, '<div style="position:relative;height:0;padding-bottom:56.25%"><iframe class="youtubeLink" width="500" rameborder="0" src="https://www.youtube.com/embed/_pwdixReIZ4"></iframe></div>');
 			});
 			it('should return false if no youtube link found',function(){
                    var data = {
