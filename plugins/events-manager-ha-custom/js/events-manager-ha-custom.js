539b8c7381bb41ccfa17fe5c957b269b5bd7205d
Messi Chaib <messi.chaib@sjsu.edu>
2017-03-17 16:11:13 -0700

    generateImg() function was added to select whatever image type the user chooses: featured image, youtube link, category image or default image if no image was chosen

--- plugins/events-manager-ha-custom/js/events-manager-ha-custom.js
+++ plugins/events-manager-ha-custom/js/events-manager-ha-custom.js
@@ -49,6 +49,69 @@ var EMHaCustom = (function($, window) {
 		
 	}
 
+
+	/** 
+	  * Generate fgenerateEntryContentLeft 
+	  * @param {Object} options - {
+	  * 			categoryImageLink: 'category image link',
+      *             featuredImageLink: 'featured image link',
+      *             youtubeLink: 'youtube link',
+      *             defaultImageLink: 'link to default image'
+	  *         } 
+	  * @return {HTML|false}
+	*/
+	public.generateEntryContentLeft=function(options){
+		
+      
+     if(options.youtubeLink){ 
+        
+ 	   	 this.generateYoutubeLink(options); 
+         options.featuredImageLink=options.youtubeLink;
+
+       }else if(options.featuredImageLink){
+		options.featuredImageLink=options.featuredImageLink;
+	  }
+ 	  else if(options.categoryImageLink){
+
+       	options.featuredImageLink=options.categoryImageLink;
+       
+       	
+       }else{
+       	 options.featuredImageLink=options.defaultImageLink;
+       }
+       
+     
+	     
+   var template = this.generateImg(options.featuredImageLink);
+
+
+       return template;
+	
+
+}
+
+
+/** 
+	  * Generate HTML
+	  * @param {Object} link - {
+	  * 			template: "image element",
+	  * 			
+	  *         } 
+	  * @return {HTML|false}
+	*/
+
+public.generateImg=function(link){
+
+var template =  '<img src=\'' + link + '\'>' ;
+
+
+	return $.parseHTML(template)[0];
+    
+}
+
+
+
+
 	/** 
 	  * Generate youtube video iframe
 	  * @param {Object} options - {
@@ -63,18 +126,18 @@ var EMHaCustom = (function($, window) {
 			console.log("ERROR: Undefined youtubeLink");
 			return false;
 		}
-
+        
 		if(options.attr_name === 'Youtube Link') {
 			
             var extractId =  options.youtubeLink.split("v=");
             var idPos = extractId.length-1;
 
             var id = extractId[idPos];
-       
+         
 
 	        var template = ['<div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/', id, '" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>'].join('');
-
-
+             
+            
 			return $.parseHTML(template)[0];			
 		} else {
 			//console.log('ERROR: Unknown attribute name', options.attr_name);
